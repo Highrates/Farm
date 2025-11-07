3738bba6b59d2ffa05a45013046f033ea46eb066
Highrates <ap7.atb@gmail.com>
2025-11-07 17:48:48 +0300

    Remove scroll lock completely to fix desktop scrolling issues

--- src/App.tsx
+++ src/App.tsx
@@ -105,32 +105,6 @@ function App() {
     }
   }, [isMobileMenuOpen]);
 
-  // Блокировка скролла при открытом меню (только на мобильных)
-  useEffect(() => {
-    if (isMobile && isMobileMenuOpen) {
-      // Сохраняем текущую позицию скролла
-      const scrollY = window.scrollY;
-      
-      // Блокируем скролл
-      document.body.style.position = 'fixed';
-      document.body.style.top = `-${scrollY}px`;
-      document.body.style.left = '0';
-      document.body.style.right = '0';
-      document.body.style.width = '100%';
-      document.body.style.overflow = 'hidden';
-      
-      return () => {
-        // Восстанавливаем при размонтировании
-        document.body.style.position = '';
-        document.body.style.top = '';
-        document.body.style.left = '';
-        document.body.style.right = '';
-        document.body.style.width = '';
-        document.body.style.overflow = '';
-        window.scrollTo(0, scrollY);
-      };
-    }
-  }, [isMobile, isMobileMenuOpen]);
 
   // Анимация заголовка героя
   useEffect(() => {
